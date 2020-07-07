d752f536d29bd007b8df8000b2b1dd4f342ac5a2
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-07-07 09:10:58 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Retirada a pasta .idea Inserido mais dados.

---
+++ manipulando_arquivos/criando_arquivo.py
@@ -0,0 +1,20 @@
+"""
+Vamos criar o arquivo2.txt
+"""
+# Usuário dará um nome para o arquivo
+nome_arquivo = input("digite um nome para o arquivo: ") # Dado o nome "teste"
+
+# Vamos dar a extensão do arquivo
+nome_arquivo = nome_arquivo + '.txt'
+
+# Vamos abrir o arquivo para escrita e inserirmos dados.
+arquivo2 = open(nome_arquivo, "w")
+arquivo2.write("Inserido esse texto")
+
+# Nunca esquecer de fechar o arquivo.
+arquivo2.close()
+
+arquivo2 = open(nome_arquivo, "r")
+print(arquivo2.read())
+
+
