73d9a9d872d48e6649b12d76fce63a0384e7c45a
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-07-10 22:36:40 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional.

---
+++ manipulando_arquivos/buscando_url.py
@@ -0,0 +1,25 @@
+"""
+Vamos fazer uma função que busca uma URL através da biblioteca:
+"requests"
+Código aprendido no curso:
+- Python Pro
+_ https://www.python.pro.br/
+Excelente cuso, indico com empenho.
+"""
+import requests
+
+
+def busca_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+    :param usuario: str com o nome de usuário no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resposta = requests.get(url)
+    return resposta.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(busca_avatar('allef'))
+
