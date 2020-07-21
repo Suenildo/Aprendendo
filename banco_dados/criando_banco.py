c9b4bbfb4f1bd4d2732cb3917f8fc336900926e8
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-07-21 07:59:06 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite.

--- banco_dados/criando_banco.py
+++ banco_dados/criando_banco.py
@@ -1,9 +1,7 @@
-
+# Vamos importar o módulo da linguagem Python que nos permite acessar o SQLite
 import sqlite3
-# sqlite é nativa no python
 
-
-# Criando uma conexão
+# Criando uma conexão,  salvando no objeto "conexao"
 # Se o banco de dados não existir, ele é criado neste momento.
 conexao = sqlite3.connect('corona.db')
 
@@ -24,11 +22,11 @@ O cursor lhe dá acesso ao banco de dados
 
 crs = conexao.cursor()
 
-# Vamos criar nossa primeira tabela no banco de Dados
-cria_tabela = 'create table dados ' \
+# Vamos criar nossa primeira tabela no banco de Dados (DDL)
+cria_tabela = 'create table if not exists dados ' \
               '(id integer primary key,' \
               'País varchar(100), ' \
-              'Casos varchar(100))'
+              'Casos integer)'
 
 '''
 Vamos chamar a função ".execute" do meu objeto crs (meu cursor),
@@ -49,3 +47,34 @@ for registros in insercao:
 '''Vamos executar o commit, para o que foi criado, ser gravado
 no banco de dados. O commit é chamado com a conexão do BD'''
 conexao.commit()
+
+# Vamos fazer uma consulta ao banco de dados criado
+
+# Vamos criar um objeto para pesquisa:
+pesquisa_1 = 'select * from dados'
+
+# Vamos executar o objeto criado com o "execute".
+# Vamos criar um objeto para salvar os registros pesquisados.
+crs.execute(pesquisa_1)
+pesquisado = crs.fetchall()
+
+# Através de um "for", vamos listar o conteúdo de "pesquisado"
+for linha in pesquisado:
+    print('Id: %d, País: %s, Casos: %d \n' % linha)
+
+
+
+
+'''
+cursor.fetchall() fetches all the rows of a query result. 
+It returns all the rows as a list of tuples. An empty list
+ is returned if there is no record to fetch.
+
+cursor.fetchmany(size) returns the number of rows specified 
+by size argument. When called repeatedly this method fetches 
+the next set of rows of a query result and returns a list of 
+tuples. If no more rows are available, it returns an empty list.
+
+cursor.fetchone() method returns a single record or None if no more
+rows are available.
+'''
