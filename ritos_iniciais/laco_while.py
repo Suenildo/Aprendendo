4c4eafe07ec31938b2962f9fbac77e25b6fffac0
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-07-28 11:42:33 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help

--- ritos_iniciais/laco_while.py
+++ ritos_iniciais/laco_while.py
@@ -2,6 +2,8 @@
 O loop while executa um conjunto de instruções enquanto uma condição
 é verdadeira. Quando essa condição passa a ser falsa, o loop é interrompida.
 """
+
+
 count = 0
 while count <= 5:
     print(count)
@@ -54,3 +56,34 @@ while pergunta <= 3:
     pergunta +=1
 
 print(f'O candidato teve {acertos} acertos ')
+
+# Vamos usar acumuladores:
+
+contador = 1
+acumulador = 0
+while contador <= 6:
+    numero = int(input(f'Digite o {contador}º número: '))
+    acumulador = acumulador + numero
+    contador += 1
+
+print(f'O valor acumulado foi: {acumulador}')
+
+# Se eu quiser saber a média aritmética:
+
+print(f' A média aritmético dos valores digitados é: {acumulador / (contador - 1)}')
+
+# Repetições aninhadas:
+
+tabuada = 1
+while tabuada <= 10:
+    numero = 1
+    while numero <= 10:
+        print(f'{tabuada} x {numero} = {tabuada * numero}')
+        numero += 1
+    tabuada += 1
+''' Exemplo tirado do livrro: 
+Introdução a Programação com Python
+Nilo Ney Coutinho Meneses
+Novatec'''
+
+
---
+++ ritos_iniciais/sistema_ajuda.py
@@ -0,0 +1,56 @@
+"""
+Este exercício foi copiado do site:
+
+https://www.youtube.com/channel/UCrWvhVmt0Qac3HgsjQK62FQ
+
+Que é um excelente canal para quem quer aprender diversos
+assuntos relacionados a programação e tecnologia.
+O nome do fundador é GUANABARA, indico com empenho para
+quem quer aprender um conteúdo de qualidade, e acreditem,
+é  DE GRAÇA.
+"""
+"""
+Exercício Python 106: Faça um mini-sistema que utilize o 
+Interactive Help do Python. O usuário vai digitar o comando 
+e o manual vai aparecer. Quando o usuário digitar a palavra 
+'FIM', o programa se encerrará. Importante: use cores.
+"""
+from time import sleep
+
+c = ('\033[m',             # sem cor
+     '\033[0;30;41m',      # vermelho
+     '\033[0;30;42m',      # verde
+     '\033[0;30;43m',      # amarelo
+     '\033[0;30;44m',      # azul
+     '\033[0;30;45m',      # roxo
+     '\033[7;30m')         # branco
+
+
+
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com} \' ', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+
+comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA PYHELP', 2)
+    comando = str(input('Função ou Biblioteca > '))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+titulo('ATÉ LOGO', 1)
