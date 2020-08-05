7a5b1fd2d111dbb92b3f94910afd101b367f8022
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-08-04 21:23:51 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension

--- ritos_iniciais/list_comprehensions.py
+++ ritos_iniciais/list_comprehensions.py
@@ -1,5 +1,40 @@
+"""
+List Comprehension é uma construção sintática disponível
+em algumas linguagens de programação para criação de uma lista
+baseada em listas existentes. Ela segue a forma da notação de
+definição de conjunto matemática como forma distinta para uso de
+funções de mapa e filtro.
+Foi concebida na PEP 202 e é uma forma concisa de criar e manipular listas.
+"""
+
 import math
 
+# Forma simples de criação de uma lista:
+z = [x for x in range(6)]
+print(z)
+print()
+
+
+# Criando uma lista com um for:
+print('Com o for:')
+q = []
+for a in range(0, 10):
+    if math.sqrt(a) % 1 == 0:
+        q.append(a)
+
+print(q)
+print()
+
+# Criando a mesma lista com list comprehension:
+print('Com List Comprehension:  ')
 q = [a for a in range(0, 10) if math.sqrt(a) % 1 == 0]
 
 print(q)
+
+'''
+Mesmo dando mais praticidade ao código, as "list comprehensions",
+devem ser usadas com moderação, pois torna o código mais complexo
+para que está lendo.
+Lempre-se:  "Readability counts".
+'''
+
