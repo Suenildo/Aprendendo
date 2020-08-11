8f05b5f692b18ca60eb9cd1ed39b490b556f0f2d
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-08-11 12:04:47 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension Acrescentado mais exemplos.

--- poo/criando_classe.py
+++ poo/criando_classe.py
@@ -8,6 +8,7 @@ de entidades do mundo real, representadas por objetos
 Origem: Wikipédia, a enciclopédia livre.
 """
 
+
 class Revista():
     """
     Criada a classe 'Revista' com a palavra reservada 'class'.
@@ -15,20 +16,33 @@ class Revista():
     Em seguida, criar dois atributos: 'titulo' e 'assunto'.
     Por último o método 'imprime'.
     """
+
     def __init__(self):
         self.titulo = 'Programação Python'
         self.assunto = 'Informática'
         print('Aqui o construtor cria o objeto')
+
     def imprime(self):
         print(f'Foi criado o objeto revista {self.titulo} '
               f'cujo assunto é {self.assunto}')
 
+
+print('Quando eu instanciar a classe com um objeto, o construtor é acionado.')
 revista1 = Revista()
 
+print()
+
+print('A quem pertense o objeto revista1, a quem foi instanciado?')
 print(type(revista1))
 
+print()
+
+print('Posso imprimir um atributo, chamando um parâmetro:')
 print(revista1.titulo)
 
+print()
+
+print('Fazendo a chamada do método imprime:')
 revista1.imprime()
 
 '''
--- poo/criando_classe_v1.py
+++ poo/criando_classe_v1.py
@@ -3,6 +3,7 @@ Criaremos, nesse código, os atributos de forma
 correta, passados como parâmetros.
 """
 
+
 class RevistaQuadrinhos():
     def __init__(self, tipo, titulo):
         self.tipo = tipo
@@ -11,10 +12,17 @@ class RevistaQuadrinhos():
     def imprime(self, tipo, titulo):
         print(f'Revista do tipo {tipo} com o título {titulo}')
 
+# Criando um objeto
 revistinha = RevistaQuadrinhos('Heróis', 'Homem Aranha')
 
+# Chamada do método imprime, com os parâmetros do objeto instanciado:
 revistinha.imprime('Heróis', 'Homem Aranha')
+print()
 print("-*-" * 16)
+print()
+
+# Criando(instanciando) outro objeto:
 revistinha1 = RevistaQuadrinhos('Infantil', 'Turma da Mônica')
 
+# Chamada do método impreme:
 revistinha1.imprime('Infantil', 'Turma da Mõnica')
