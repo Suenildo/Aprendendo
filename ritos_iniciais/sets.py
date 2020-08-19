f729451951351a0d93556f84209077cc8b1fa200
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-08-18 21:40:52 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension Acrescentado mais exemplos.

---
+++ ritos_iniciais/sets.py
@@ -0,0 +1,13 @@
+"""
+Os sets são uma coleção de itens desordenada, parcialmente
+imutável e que não podem conter elementos duplicados.
+Por ser parcialmente imutável, os sets possuem permissão
+das operações de  adição e remoção de elementos.
+"""
+
+# Vamos declarar um set:
+set_01 = {2, 4, 6, 8}
+print(type(set_01))
+
+set_02 = set([1, 2, 8, 9, 10])
+print(type(set_02))
