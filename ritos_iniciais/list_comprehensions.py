09c098ba76a02c10a4f764178c0e1dbe6b5d40ca
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-08-20 07:10:31 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension Acrescentado mais exemplos.

--- ritos_iniciais/list_comprehensions.py
+++ ritos_iniciais/list_comprehensions.py
@@ -14,7 +14,6 @@ z = [x for x in range(6)]
 print(z)
 print()
 
-
 # Criando uma lista com um for:
 print('Com o for:')
 q = []
@@ -31,10 +30,51 @@ q = [a for a in range(0, 10) if math.sqrt(a) % 1 == 0]
 
 print(q)
 
+
+# Você pode fazer chamada de funções na comprehension:
+def titularizar(palavra):
+    palavra = palavra.replace(palavra[0], palavra[0].upper())
+    return palavra
+
+
+''' Nessa função, se a palavra tiver outra letra igual 
+a do índice "0", ela a colocará como maiúscula também.
+Cuidado com esse tipo de erro.'''
+
+
+paises = ['brasil', 'espanha', 'bolívia', 'paraguai']
+corrigido = [titularizar(paises) for paises in paises]
+
+print(corrigido)
+
+print()
+print('*=*' * 15)
+
+print("Uma cusiosidade:")
+''' Em python:
+ - 0 é sempre falso por padrão; e
+ - 1 é sempre verdadeiro por padrão.
+Então se você faz uma operação que tem como retorno 0, o python,
+por padrão vai considerá-la falsa, veja um exemplo clássico:'''
+
+valores = [1, 2, 3, 4, 5, 6, 7, 8]
+
+numero = [valor for valor in valores if valor % 2]
+print(numero)
+
+''' Você achou que ele retornaria os valores páres, mas não,
+ele retornou os ímares, por que:
+Isso aconteceu porque como o módulo de um númeo par por 2
+dá resultado 0, o pyhon o clasificou como False, para retornar
+os pares, faremos uma modificação na "comprehencion".'''
+
+numero_pares = [valor for valor in valores if not valor % 2]
+print(numero_pares)
+
+
 '''
 Mesmo dando mais praticidade ao código, as "list comprehensions",
 devem ser usadas com moderação, pois torna o código mais complexo
 para que está lendo.
 Lempre-se:  "Readability counts".
 '''
-
