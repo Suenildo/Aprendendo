b527ce39dab0b331036919020d5a83b3a8adbbb9
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-08-21 06:25:51 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension Acrescentado mais exemplos. Adicionando mais coleções.

---
+++ ritos_iniciais/named_tuple.py
@@ -0,0 +1,31 @@
+"""
+Esse métodoretorna uma nova subclasse de tupla chamada typename.
+A nova subclasse é usada para criar objetos semelhantes a tuplas
+que possuem campos acessíveis por pesquisa de atributo, além de
+serem indexáveis e iteráveis.
+"""
+from collections import namedtuple
+
+# Como podemos declarar:
+
+# Declaração mais comum:
+jogador = namedtuple('jogador', ['nome', 'idade', 'time'])
+jogador_1 = jogador(nome='Neymar', idade=25, time='Barcelona')
+
+print(jogador_1)
+
+print()
+
+# A partir da declaração, podemos acessar os parâmetros
+print(jogador_1[0])
+print(jogador_1[1])
+print(jogador_1[2])
+print(f'Jogador: {jogador_1[0]}, idade {jogador_1[1]}, seu time {jogador_1[2]}')
+
+print()
+
+# OU
+print(jogador_1.nome)
+print(jogador_1.idade)
+print(jogador_1.time)
+print(f'Jogador: {jogador_1.nome}, idade {jogador_1.idade}, seu time {jogador_1.time}')
