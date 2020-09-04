42b2681f5a30afdbc6f299350596f8a2e20390ee
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-09-04 05:50:25 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension Acrescentado mais exemplos. Adicionando mais coleções. Funções novas. Mais uma função.

--- programacao_funcional/funcoes.py
+++ programacao_funcional/funcoes.py
@@ -1,2 +1,37 @@
+"""
+Função com vários argumentos definidos de forma
+genérica com (*args)
+O (*args) devolve uma tupla contendo os argumentos.
+
+Os exemplos vão levar em consideração somente números.
+"""
 
 
+def multiplica(*args):
+    if args == 0:
+        return 0
+    produto = 1
+    for fator in args:
+        produto = produto * fator
+    return produto
+
+
+print(multiplica(0))
+print(multiplica(2, 3))
+print(multiplica(2, 3, 5))
+print(multiplica(2, 3, 5, 7))
+
+print()
+print('=*=' * 10)
+
+# Podemos notar que eu posso passar quantos argumentos eu quiser.
+
+'''Como o (*args) retorna, no seu tipo, uma tupla com os valores,
+se nós fizermos uma função soma, ficaria assim:'''
+
+def soma(*args):
+    return sum(args)
+
+
+
+print(soma(1, 5, 9, 11))
