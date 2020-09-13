a0e00d8aa3a35bffe65729720df286e828716a91
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-09-12 22:33:28 -0300

    Inicializado o repositório. Criados os pacotes: - Manipulando Arquivos e - Ritos iniciais. Inserido mais dados. Inserido mais códigos de manipulação de dados. Inserido o pacote programação funcional. Criado o pacote POO. Criado mais alguns arquivos em POO. Iniciando Banco de Dados. Iniciaremos com SQLite. Acrescentado mais códigos. Criado o código Help Criado o pacote gráficos: Acrescentando mais modelos de gráficos. List Comprehension Acrescentado mais exemplos. Adicionando mais coleções. Funções novas. Mais uma função. Reformulando conteúdo.

--- programacao_funcional/funcao_lambda.py
+++ programacao_funcional/funcao_lambda.py
@@ -9,30 +9,25 @@ O resultado é uma nova expressão expr_ret.
 valor = [1, 2, 3, 4, 5]
 valor2 = [1, 2, 3, 4, 5]
 
-
 print('Dobrando e triplicando o valor dos elementos da lista')
-valor_dobro = map(lambda i: i*2, valor)
+valor_dobro = map(lambda i: i * 2, valor)
 valor_dobro = list(valor_dobro)
 print(valor_dobro)
-valor_triplo = map(lambda j: j*3, valor)
+valor_triplo = map(lambda j: j * 3, valor)
 valor_triplo = list(valor_triplo)
 print(valor_triplo)
 
 print()
 print('Elevando ao quadrado o valor dos elementos da lista')
-valor_quadrado = map(lambda i: i**2, valor2)
+valor_quadrado = map(lambda i: i ** 2, valor2)
 valor_quadrado = list(valor_quadrado)
 print(valor_quadrado)
 
 # pode ser assim também:
+print('=*=' * 11)
 
 nome_estado = lambda nome, sigla: nome.strip().title() + ' ' + sigla.upper()
 
 print(nome_estado('paraíba', 'pB'))
 
-''' Repare que na função (nome_estado), nós colocamos unas funções para
-correção de possíveis erros cometidos pelos usuários:
-- strip()
-- title()
-- upper()
-'''
+
