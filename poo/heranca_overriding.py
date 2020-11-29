a8bc231f3f8252dd6af20ba0511363ca5205ebf8
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-11-29 18:59:55 -0300

    mais sobre Herança.

--- poo/heranca_overriding.py
+++ poo/heranca_overriding.py
@@ -1,4 +1,8 @@
-# Vamos definir uma Super Classe:
+"""
+Vamos reescrever um métoda da Super Classe
+em uma classe filha.
+"""
+
 class Pessoa:
     def __init__(self, nome, sobrenome, cpf):
         self.nome = nome
@@ -9,21 +13,12 @@ class Pessoa:
         return f'{self.nome} {self.sobrenome}'
 
 
-#  Vamos definir as Classes Filhas:
 class Cliente(Pessoa):
     """Cliente herda de Pessoa"""
 
     def __init__(self, nome, sobrenome, cpf, renda):
         Pessoa.__init__(self, nome, sobrenome, cpf)  # Aqui  tem (self)
         self.__renda = renda
-
-
-'''
- Se eu chamar a Super classe pelo nome dela, eu tenho que 
-usar o (((SELF))), mas não é o método comum de se chamar a Super
-Classe, abaixo, na Classe Funcionário, vamos chamar com o modo
-correto.
-'''
 
 
 class Funcionario(Pessoa):
@@ -33,15 +28,10 @@ class Funcionario(Pessoa):
         super().__init__(nome, sobrenome, cpf)  # Aqui não tem (self)
         self.__matricula = matricula
 
+    def nome_completo(self):
+        print(super().nome_completo())  # (*)
+        return f'Funcionário: {self.__matricula} Nome: {self.nome}'  # Overriding
 
-''' 
-super().__init__(nome, sobrenome, cpf) - chama os argumentos 
-da classe Mãe. (((((Sem o self))))
-Esses agumentos serão utilizados nas classes filhas.
-Chama também o método: nome_completo.
-'''
-
-# Instanciando objetos:
 
 cliente_1 = Cliente('Juvenal', 'souza', '000.000.111-23', 1000)
 funcionario_1 = Funcionario('Nelson', 'pereira', '000.000.122-23', 1234)
@@ -49,8 +39,6 @@ funcionario_1 = Funcionario('Nelson', 'pereira', '000.000.122-23', 1234)
 print(cliente_1.nome_completo())
 print(funcionario_1.nome_completo())
 
-print()
-
-# Vamos chamar os dicionários das Classes filhas:
-print(cliente_1.__dict__)
-print(funcionario_1.__dict__)
+'''
+(*) - Com o Super, eu posso acessar o método que eu quiser na Super Classe.
+'''
