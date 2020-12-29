ecd89a931bf0875871a5684ee8249e90b4a37052
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-12-29 06:54:55 -0300

    Herança múltipla.

--- poo/heranca_multipla.py
+++ poo/heranca_multipla.py
@@ -8,4 +8,58 @@ A herança multipla pode ser feita das seguintes formas:
  - multiderivação Indireta
 """
 
+class Animal:
+    def __init__(self, nome):
+        self.__nome = nome
 
+    def cumprimentar(self):
+        return f'Eu sou {self.__nome}'
+
+
+class Aquatico(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def nadar(self):
+        return f'{self._Animal__nome} está nadando'
+
+    def cumprimentar(self):
+        return f'Eu sou {self._Animal__nome} do mar'
+
+
+class Terrestre(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def andar(self):
+        return f'{self._Animal__nome} está andando.'
+
+    def cumprimentar(self):
+        return f'Eu sou {self._Animal__nome} da terra'
+
+
+class Pinguim(Aquatico, Terrestre):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+
+# Instanciando e testando:
+
+baleia = Aquatico('Moob')
+print(baleia.nadar())
+print(baleia.cumprimentar())
+
+print()
+
+tatu = Terrestre('Tata')
+print(tatu.andar())
+print(tatu.cumprimentar())
+
+print()
+
+wily = Pinguim('Wily')
+print(wily.andar())
+print(wily.nadar())
+print(wily.cumprimentar())
